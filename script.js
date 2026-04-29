dc714c65d63c62a54304379d1c53b5e9bd3f80e3
isaonline <isapaesnascimento@gmail.com>
2026-04-29 20:04:31 -0300

    Adicionar tasks em localstorage

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const STORAGE_KEY = "dadosDeTask"
+
 const barraMenu = document.getElementById('tab')
 const buttonFoco = document.getElementById('foco')
 const buttonPausa = document.getElementById('pausa')
@@ -22,6 +24,10 @@ const labelMusica = document.getElementById('label-musica')
 const illustEmptyState = document.getElementById('illust-tasks-vazias')
 const buttonAddTask = document.getElementById('button-adicionar-task')
 const formTask = document.getElementById('task-form')
+const inputTask = document.getElementById('task-input')
+const buttonSalvarTask = document.getElementById('button-salvar')
+const buttonCancelar = document.getElementById('button-cancelar')
+const listaDeTasks = document.getElementById('tasks-list')
 
 function removerOutrosModos() {
     htmlBody.classList.remove('modo-foco', 'modo-pausa', 'modo-descanso')
@@ -111,6 +117,55 @@ function tocarMusica() {
     }
 }
 
+function renderizarLocalStorage() {
+    try {
+        const dadoBruto = localStorage.getItem(STORAGE_KEY)
+        if (!dadoBruto) {
+            return
+        }
+
+        let dados 
+        try {
+            dados = JSON.parse(dadoBruto)
+        } catch (e) {
+            alert('Formato JSON inválido no localStorage! ', e)
+            return
+        }
+
+        listaDeTasks.innerHTML = ''
+        dados.forEach(task => {
+            const div = document.createElement('div')
+            div.className = 'task'
+            const span = document.createElement('span')
+            span.className = 'check-task'
+            const h4 = document.createElement('h4')
+            h4.textContent = task
+            h4.className = 'texto-text'
+            const img = document.createElement('img')
+            img.src = 'icons/pencil.svg'
+
+            div.appendChild(span)
+            div.appendChild(h4)
+            div.appendChild(img)
+            
+            listaDeTasks.appendChild(div)
+        })
+
+    } catch (err) {
+        alert('Erro ao ler o localStorage :( Tipo do erro:', err)
+    }
+}
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    console.log(localStorage.getItem(STORAGE_KEY))
+    if (localStorage.length != 0) {
+        renderizarLocalStorage()
+        listaDeTasks.classList.remove('hidden')
+        listaDeTasks.classList.remove('task-list-vazia')
+        illustEmptyState.classList.add('hidden')
+    }
+})
+
 buttonFoco.addEventListener('click', () => {
     trocarModo('foco')
 })
@@ -160,7 +215,30 @@ labelMusica.addEventListener('click', () => {
     tocarMusica()
 })
 
+buttonCancelar.addEventListener('click', () => {
+    if (dadosSalvos == null) {
+        illustEmptyState.classList.remove('hidden')
+    }
+    formTask.classList.add('hidden')
+    inputTask.textContent = ''
+})
+
 buttonAddTask.addEventListener('click', () => {
     illustEmptyState.classList.add('hidden')
     formTask.classList.remove('hidden')
+})
+
+formTask.addEventListener('submit', (event) => {
+    event.preventDefault()
+    let tasksExistentes = JSON.parse(localStorage.getItem(STORAGE_KEY)) || []
+    let novaTask = inputTask.value.trim()
+    tasksExistentes.push(novaTask)
+    if (novaTask.length != 0) {
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(tasksExistentes))
+        renderizarLocalStorage()
+        formTask.classList.add('hidden')
+        inputTask.textContent = ''
+    } else {
+        alert('A descrição da task está vazia! Nada foi salvo.')
+    }
 })
